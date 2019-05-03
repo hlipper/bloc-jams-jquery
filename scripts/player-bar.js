9f3a57444833ab5c53e5343eba8801ef768dd181
Holly Lipper <hollylipper@gmail.com>
2019-05-02 19:17:35 -0500

    Checkpoint 10 Assignment Complete.

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -12,4 +12,14 @@ $('button#next').on('click', function () {
   const nextSong = album.songs[nextSongIndex];
   player.playPause(nextSong);
 });
+
+$('button#previous').on('click', function () {
+  if (player.playState !== 'playing') {return;}
+  const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+  const previousSongIndex = currentSongIndex -1;
+  if (previousSongIndex < 0) {return;}
+  const previousSong = album.songs[previousSongIndex];
+  player.playPause(previousSong);
+});
+
 }
